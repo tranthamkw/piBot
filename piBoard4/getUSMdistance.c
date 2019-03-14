fbd4fc2abe767bf109f30d3b812da588590d0d38
Ken <tranthamkw@unk.edu>
2019-03-14 12:10:51 -0500

    testing USM

--- piBoard4/getUSMdistance.c
+++ piBoard4/getUSMdistance.c
@@ -15,33 +15,42 @@ int main(int argc, char **argv) {
 
 	unsigned int value;
 	int status, i;
-//	unsigned int distance[8];
+	unsigned int distance[9];
+
+for (i=0;i<9;i++){
+	distance[i]=0;
+}
+printf("Measuring ...\n");
 
 	initializeBoard();
 
-	printf("angle\tTOF\n");
-	for (i=4;i>0;i--){
+	for (i=4;i>=0;i--){
 		setRS485ServoPosition(USM,0,i);
 		status = getRS485USMdistance(USM,&value);
 		if (status) value=0;
-		printf("%d\t%d\t%d\n",i,value,value*34/500);
-		delay(40);
+		distance[i]+=value*34/500;
+		delay(20);
 }
 	for (i=0;i<9;i++){
 		setRS485ServoPosition(USM,0,i);
 		status = getRS485USMdistance(USM,&value);
 		if (status) value=0;
-		printf("%d\t%d\t%d\n",i,value,value*34/500);
-		delay(40);
+		distance[i]+=value*34/500;
+		delay(20);
 	}
-	for (i=7;i>4;i--){
+	for (i=8;i>4;i--){
 		setRS485ServoPosition(USM,0,i);
 		status = getRS485USMdistance(USM,&value);
 		if (status) value=0;
-		printf("%d\t%d\t%d\n",i,value,value*34/500);
-		delay(40);
+		distance[i]+=value*34/500;
+		delay(20);
 }
 
+printf("angle\td(cm)\n");
+
+for (i=0;i<9;i++){
+	printf("%d\t%d\n",i,(distance[i]/2));
+}
 printf("\n");
 
 //return to position 4
